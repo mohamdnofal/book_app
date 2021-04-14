9a7a65b8315244f8702df7f94ff8de97c65943a9
Mohammad Nofal <m7madmody@gmail.com>
2021-04-15 02:02:47 +0300

    almost done

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const server = express();
 
 const superagent = require('superagent');
 
+const pg = require('pg');
+
 const PORT = process.env.PORT || 3000;
 
 server.set('view engine','ejs');
@@ -18,21 +20,38 @@ server.use(express.urlencoded({extended:true}));
 server.get( '/', homeRoute );
 server.get( '/searches/new', searchRoute );
 server.post( '/searches',booksRoute );
+server.get( '/books/:id',booksDetails );
+server.post( '/books' , choseBook );
 
-function homeRoute (req,res){
+const client = new pg.Client( {connectionString: process.env.DATABASE_URL,
+  // ssl: {
+  //   rejectUnauthorized : false
+  // }
+});
 
-  res.render('pages/index');
+
+function homeRoute( req,res ){
+  let SQL=`SELECT * FROM books ;`;
+  client.query(SQL)
+    .then (bookAppData=>{
+      let bookCount = bookAppData.rows.length;
+
+      res.render('pages/index',{books:bookAppData.rows,count:bookCount});
+    })
+    .catch(error=>{
+      res.send(error);
+
+      res.render('pages/searches/error', { errors: error });
+    });
 
 }
 
+
 function searchRoute (req,res){
-
   res.render('pages/searches/new');
-
 }
 
 function booksRoute (req,res){
-
   let search = req.body.book;
   let bookAuthorURL;
 
@@ -44,31 +63,57 @@ function booksRoute (req,res){
     bookAuthorURL = `https://www.googleapis.com/books/v1/volumes?q=inauthor:${search}`;
   }
   superagent.get(bookAuthorURL)
-    .then(fullBookData => {
-      let bookData = fullBookData.body.items;
+    .then(allBooks => {
+      let bookData = allBooks.body.items;
 
-      let bookObjArr = bookData.map(item => {
+      let bookArr = bookData.map(item => {
         return new Book (item); });
 
-      res.render('pages/searches/show',{renderBookData:bookObjArr} );
+      res.render('pages/searches/show',{renderBookData:bookArr} );
     })
     .catch(error => {
       res.render('pages/searches/error', { errors: error });
     });
 }
 
-function Book (oneBook){
+function Book (bookData){
 
-  this.title = oneBook.volumeInfo.title ? oneBook.volumeInfo.title : 'Unkonown',
-  this.authors = oneBook.volumeInfo.authors? oneBook.volumeInfo.authors : 'Unkonown',
-  this.description = oneBook.volumeInfo.description ? oneBook.volumeInfo.description : 'Unkonown' ,
-  this.imgUrl = oneBook.volumeInfo.imageLinks? oneBook.volumeInfo.imageLinks.thumbnail : 'https://i.imgur.com/J5LVHEL.jpg';
+  this.title = bookData.volumeInfo.title ? bookData.volumeInfo.title : 'Unkonown',
+  this.authors = bookData.volumeInfo.authors? bookData.volumeInfo.authors : 'Unkonown',
+  this.description = bookData.volumeInfo.description ? bookData.volumeInfo.description : 'Unkonown' ,
+  this.imgUrl = bookData.volumeInfo.imageLinks? bookData.volumeInfo.imageLinks.thumbnail : 'https://i.imgur.com/J5LVHEL.jpg';
+}
+
+function booksDetails (req,res){
+  let SQL=`SELECT * FROM books WHERE id=$1;`;
+  let safeValues=[req.params.id];
+  client.query(SQL,safeValues)
+    .then(results=>{
+      res.render('pages/books/detail',{detail:results.rows[0]});
+    });
+
+}
+
+function choseBook (req,res){
+  let {auther,title,isbn,image_url,description}=req.body;
+  let SQL=`INSERT INTO books (author,title,isbn,image_url,description)VALUES ($1,$2,$3,$4,$5) RETURNING *;`;
+  let safeValues=[auther,title,isbn,image_url,description];
+  client.query(SQL,safeValues)
+    .then(results =>{
+
+      res.redirect(`/books/${results.rows[0].id}`);
+    });
 }
 
 server.get('*',(req,res)=>{
   res.send('Error');
 });
 
-server.listen(PORT,()=>{
-  console.log(`Listening on PORT ${PORT}`);
-});
+client.connect()
+  .then(() => {
+    server.listen(PORT, () =>
+      console.log(`listening on ${PORT}`));
+
+  }).catch(error=>{
+    console.log(error);
+  });
